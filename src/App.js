d7496435c5bb999bb846c0621dae6e0c08ca70c6
NalineeShinde <shindenalinee96@gmail.com>
2023-09-03 23:30:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import useClipboard from "react-use-clipboard";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { useState } from "react";
 
 function App() {
+  const [textCopied, settextCopied] = useState();
+  const [isCopied, setCopied] = useClipboard(textCopied);
+  const stopListening = () => SpeechRecognition.stopListening();
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a React hook that converts speech from the microphone to text
+          and make it available to your React componant.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="main-content" onClick={() => settextCopied(transcript)}>
+          <p>{transcript}</p>
+        </div>
+        <div className="btn-style">
+          <button onClick={setCopied} className="btn btn-success">
+            Was it copied? {isCopied ? "Yes! 👍" : "Nope! 👎"}
+          </button>
+          {/* <button onClick={setCopied} className="btn btn-success ms-5">Copy To Clipboard</button> */}
+          <button onClick={startListening} className="btn btn-danger">
+            Start Listening
+          </button>
+          <button onClick={stopListening} className="btn btn-warning ">
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
